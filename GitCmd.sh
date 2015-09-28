4e4bab451c1785eafd07ebd7348dd8a09c4556c8
Pravinkumar Hirmukhe <pravinkumar.hirmukhe@gmail.com>
2015-09-29 01:32:37 +0530

    Bachatgat update to git

--- GitCmd.sh
+++ GitCmd.sh
@@ -1,5 +1,5 @@
-git pull git@github.com:pravinhirmukhe/bachatgat.git
+git pull https://github.com/pravinhirmukhe/bachatgat.git
 git add .
 git commit -m "Bachatgat update to git"
-git push git@github.com:pravinhirmukhe/bachatgat.git
+git push https://github.com/pravinhirmukhe/bachatgat.git
 exit
